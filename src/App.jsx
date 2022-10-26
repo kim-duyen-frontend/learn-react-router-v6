d879421d775eb076ad6a81ede78ec67e9142e7f5
kim-duyen-frontend <ntduyen255@gmail.com>
2022-10-26 14:23:29 +0700

    move login to page and install redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Login from './components/Login';
+import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/not-found/NotFound";
 import PrivateRouter from './components/PrivateRouter';
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import "./login.scss";
+import "./styles.scss";
 
 function Login() {
     const [formData, setFormData] = useState({
